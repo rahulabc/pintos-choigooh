5a7a27f1c608f6f654a3c427f7410d3c754b516d
celestarry@naver.com <celestarry@naver.com@81ee49a2-cf44-11de-a697-fbeabf64b1f8>
2009-11-19 11:50:46 +0000

    dsdf

--- trunk/src/vm/frame.h
+++ trunk/src/vm/frame.h
@@ -9,3 +9,13 @@ struct frame
 };
 
 struct list frame_table;
+
+void frame_table_init();
+bool install_frame(void* upage, void* kpage, uint32_t* pd);
+void add_frame(void* kpage);
+void remove_frame(void* kpage);
+void destroy_frame(uint32_t * pd);
+void* get_user_frame(bool zero);
+void free_user_frame(void *kpage);
+void unmap_user_frame(void *kpage);
+void eviction();
